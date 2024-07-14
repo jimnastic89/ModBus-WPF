96fee5090a33be128935523e2f090f9f3049c793
james <jamesallen989@gmail.com>
2024-07-14 13:07:33 +1000

    Initial commit -Updated to .NET 4.8 -Remade UI in XAML -References to WinForms controls changed to static objects with UI bindings

---
+++ ModbusSampleCommon/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace ModbusTester
+{
+    public static class Data
+    {
+        public enum DataFormat { Bits, Bytes, Words }
+        public static MainWindow Current;
+        public static string IPAddress = "10.0.0.14";
+        public static string Unit = "3";
+        public static string StartAddress = "30051";
+        public static string Size = "100";
+        public static bool IsConnected_bool = false;
+        public static List<KeyValuePair<string, string>> Data_TextBox_List = Enumerable.Range(1, 64)
+            .Select(r => new KeyValuePair<string, string>($"{r}", $""))
+            .ToList();
+        public static DataFormat ShowDataAs = DataFormat.Bytes;
+    }
+}
---
+++ ModbusSampleCommon/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Modbus;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModbusTester
+{
+    public partial class MainWindow : UserControl, INotifyPropertyChanged
+    {
+        public string IPAddress { get => Data.IPAddress; set => SetValue(ref Data.IPAddress, value); }
+        public string Unit { get => Data.Unit; set => SetValue(ref Data.Unit, value); }
+        public string StartAddress { get => Data.StartAddress; set => SetValue(ref Data.StartAddress, value); }
+        public string Size { get => Data.Size; set => SetValue(ref Data.Size, value); }
+        public List<KeyValuePair<string, string>> Data_TextBox_List { get => Data.Data_TextBox_List; set => SetValue(ref Data.Data_TextBox_List, value); }
+        public Data.DataFormat ShowDataAs { get => Data.ShowDataAs; set => SetValue(ref Data.ShowDataAs, value); }
+        public bool IsConnected_bool { get => Data.IsConnected_bool; set => SetValue(ref Data.IsConnected_bool, value); }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Data.Current = this;
+        }
+
+        #region SetValue
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            if (propertyName != null)
+                OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            frmStart.Current.btnConnect_Click(sender, e);
+        }
+
+        private void ReadCoils_Click(object sender, RoutedEventArgs e)
+        {
+            frmStart.Current.btnReadCoils_Click(sender, e);
+        }
+
+        private void ReadHoldingRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine($"IP: {IPAddress} Unit: {Unit} StartAddress: {StartAddress} Size: {Size}");
+            frmStart.Current.btnReadHoldReg_Click(sender, e);
+        }
+
+        private void ReadDiscreteInputs_Click(object sender, RoutedEventArgs e)
+        {
+            frmStart.Current.btnReadDisInp_Click(sender, e);
+        }
+
+        private void WriteMultipleRegisters_Click(object sender, RoutedEventArgs e)
+        {
+            frmStart.Current.btnWriteMultipleReg_Click(sender, e);
+        }
+
+        private void WriteMultipleCoils_Click(object sender, RoutedEventArgs e)
+        {
+            frmStart.Current.btnWriteMultipleCoils_Click(sender, e);
+        }
+
+        private void WriteSingleRegister_Click(object sender, RoutedEventArgs e)
+        {
+            frmStart.Current.btnWriteSingleReg_Click(sender, e);
+        }
+
+        private void WriteSingleCoil_Click(object sender, RoutedEventArgs e)
+        {
+            frmStart.Current.btnWriteSingleCoil_Click(sender, e);
+        }
+
+        private void ReadInputRegister_Click(object sender, RoutedEventArgs e)
+        {
+            frmStart.Current.btnReadInpReg_Click(sender, e);
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            switch (radioButton.Content)
+            {
+                case "Bits":
+                    ShowDataAs = Data.DataFormat.Bits;
+                    break;
+                case "Bytes":
+                    ShowDataAs = Data.DataFormat.Bytes;
+                    break;
+                case "Words":
+                    ShowDataAs = Data.DataFormat.Words;
+                    break;
+            }
+            if (frmStart.Current != null)
+                frmStart.Current.ShowAs(sender, e);
+        }
+    }
+}
